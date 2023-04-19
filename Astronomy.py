922fa23433f347f6359faa03db0800b69ae1dc33
Hirdesh Kumar Yadav <125953445+kumarsam1503@users.noreply.github.com>
2023-04-19 07:52:41 +0530

    Update Astronomy.py

--- Astronomy.py
+++ Astronomy.py
@@ -1,51 +1,83 @@
 import sqlite3
 import tkinter as tk
+from tkinter import ttk
+from tkinter import *
+from tkinter.font import Font
 
-# Connect to the database
-conn = sqlite3.connect('astronomy.db')
-
-# Create a function to search for an object in the database
-
-
-def search_object():
-    # Get the object name from the entry widget
-    object_name = object_name_entry.get()
-
-    # Execute a SELECT statement to search for the object name in the database
-    c = conn.cursor()
-    c.execute("SELECT * FROM objects WHERE name=?", (object_name,))
-    object_data = c.fetchone()
-
-    # Check if the object was found
-    if object_data is None:
-        result_label.config(text="Sorry, the object was not found in the database.")
-    else:
-        # Print the object information
-        name, mass, diameter, density, gravity, escape_velocity, rotation_period, length_of_day, distance_from_sun, orbital_period, orbital_velocity, moons, ring_system, global_magnetic_field, about =object_data
-        name, type, distance, diameter, mass = object_data
-        result_label.config(text=f"Name: {name}\nMass (10^24 kg): {mass}\nDiameter (km): {diameter}\nDensity (kg/m^3): {density}\nGravity (m/s^2): {gravity}\nEscape Velocity (km/s): {escape_velocity}\nRotation Period (hours):{rotation_period}\nLength of Day (hours):{length_of_day}\nDistance from Sun (10^6 km):{distance_from_sun}\nOrbital Period (days):{orbital_period}\nOrbital Velocity (km/s):{orbital_velocity}\nNumber of Moons:{moons}\nRing System?:{ring_system}\nGlobal Magnetic Field?:{global_magnetic_field}\nAbout:{about}")
-
-# Create the GUI window
+# Define the GUI
 root = tk.Tk()
-root.title("Astronomy Object Search")
+root.title("Astronomy Database Search")
 root.geometry("1400x700")
+root.configure(bg="green")
 
-# Create the object name label and entry widget
-object_name_label = tk.Label(root, text="Enter the name of the astronomical object:")
-object_name_label.pack()
-object_name_entry = tk.Entry(root, width=130, borderwidth=3)
-object_name_entry.pack()
 
-# Create the search button
-search_button = tk.Button(root, text="Search", command=search_object)
-search_button.pack()
+# Define the database connection
+conn = sqlite3.connect("astronomy.db")
+c = conn.cursor()
 
-# Create the result label to display the object information
-result_label = tk.Label(root, text="")
-result_label.pack()
+# Define the function to search the database
 
-# Run the GUI loop
+
+def search_database():
+    # Get the search query from the search box
+    search_query = search_box.get()
+
+    # Execute the SQL query to search for the data
+    c.execute("SELECT * FROM planets WHERE name LIKE ?", ('%' + search_query + '%',))
+    results = c.fetchall()
+
+    # Clear the previous results from the results treeview
+    for i in result_treeview.get_children():
+        result_treeview.delete(i)
+
+    # Display the new results in the results treeview
+    for result in results:
+        result_treeview.insert('', tk.END, values=result)
+
+# Define the search box
+search_label = tk.Label(root, text="Enter Name Astronomical Object(Planet):", borderwidth=5, foreground="Red", bg="skyblue", font="Bold")
+search_label.pack(pady=20)
+
+search_box = tk.Entry(root, width=130, borderwidth=5, foreground="Red", bg="Skyblue", font="Strong")
+search_box.pack()
+
+# Define the search button
+search_button = tk.Button(root, text="Search", bg="Black", foreground="White", font="Strong", command=search_database)
+search_button.pack(pady=20)
+
+
+# Define the results treeview
+result_treeview = ttk.Treeview(root, height=20,  columns=(
+    "Planet", "Mass (10^24 kg)", "Diameter (km)", "Density (kg/m^3)", "Gravity (m/s^2)",
+    "Escape Velocity (km/s)", "Rotation Period (hours)", "Length of Day (hours)",
+    "Distance from Sun (10^6 km)", "Orbital Period (days)", "Orbital Velocity (km/s)",
+    "Number of Moons", "Ring System?", "Global Magnetic Field?", "About"), show='headings', style='Treeview', selectmode='browse')
+
+# root.tk_setPalette(background='Red')
+header_font = Font(family='Times New Roman', size=12, weight='bold')
+style = ttk.Style(root)
+style.configure('Treeview.Heading', font=header_font)
+result_treeview.heading("Planet", text="Planet")
+result_treeview.heading("Mass (10^24 kg)", text="Mass(10^24 kg)")
+result_treeview.heading("Diameter (km)", text="Diameter(km)")
+result_treeview.heading("Density (kg/m^3)", text="Density(kg/m^3)")
+result_treeview.heading("Gravity (m/s^2)", text="Gravity(m/s^2)")
+result_treeview.heading("Escape Velocity (km/s)", text="Escape Velocity(km/s)")
+result_treeview.heading("Rotation Period (hours)", text="Rotation Period(hours)")
+result_treeview.heading("Length of Day (hours)", text="Length of Day(hours)")
+result_treeview.heading("Distance from Sun (10^6 km)", text="Distance from Sun(10^6 km)")
+result_treeview.heading("Orbital Period (days)", text="Orbital Period(days)")
+result_treeview.heading("Orbital Velocity (km/s)", text="Orbital Velocity(km/s)")
+result_treeview.heading("Number of Moons", text="Number of Moons")
+result_treeview.heading("Ring System?", text="Ring System?")
+result_treeview.heading("Global Magnetic Field?", text="Global Magnetic Field?")
+result_treeview.heading("About", text="About")
+
+result_treeview.pack(pady=10)
+
+
+# Start the GUI main loop
 root.mainloop()
 
-# Close the database connection
+# Close the database connection when the GUI is closed
 conn.close()
